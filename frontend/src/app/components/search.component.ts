09f4813d73dddaf487feac345b587c550e7b9bac
Adriano Marra <adrianomarra90@gmail.com>
2019-06-16 14:08:41 -0500

    Create select component + change the structure of search component + add two new services needed for getting the data from the API

--- frontend/src/app/components/search.component.ts
+++ frontend/src/app/components/search.component.ts
@@ -1,68 +1,87 @@
-import { Component } from '@angular/core';
-
+import { Component, Output } from '@angular/core';
+import { from } from 'rxjs';
+import { filter } from 'rxjs/operators';
 @Component({
-  selector: 'app-search',
-  template: `
+selector: 'app-search',
+template: `
+<div class="px-5">
+    <form class="needs-validation" novalidate>
+        <div class="form-row">
+            <div class="col-md-5 mb-3">
+                <label for="validationCustomUsername">What are you looking for?</label>
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroupPrepend">
+                            <i class="fa fa-search"></i>
+                        </span>
+                    </div>
+                    <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+                </div>
+            </div>
+            <div class="col-md-4 mb-3">
+                <label for="location">Location</label>
+                <input type="text" class="form-control" id="location" placeholder="Location">
+            </div>
+            <div class="col-md-3 mb-3">
+                <label for="validationCustom02">Worker specialty</label>
+                <app-select [btnText]=specialtiesText [dropDownOptions]=specialtiesOptions></app-select>
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="col-md-6 mb-3">
+                <label for="validationCustom03">Hour rate range</label>
+
+                <div class="w-100">
+
+                    <div class="col-md-6 float-left pl-0">
+                        <div ngbDropdown class="d-inline-block w-100">
+                            <app-select [btnText]=minPriceText [dropDownOptions]=priceOptions (valueChange)='calcMaxPriceOptions($event)'></app-select>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 float-left pl-0">
+                        <div ngbDropdown class="d-inline-block w-100">
+                            <app-select [btnText]=maxPriceText [dropDownOptions]=maxPriceOptions deactivate="true"></app-select>
+                        </div>
+                    </div>
+                </div>
 
 
-  <div class="px-5">
-  <form class="needs-validation" novalidate>
-      <div class="form-row">
-          <div class="col-md-4 mb-3">
-              <label for="validationCustomUsername">What are you looking?</label>
-              <div class="input-group">
-                  <div class="input-group-prepend">
-                      <span class="input-group-text" id="inputGroupPrepend">
-                          <i class="fa fa-search"></i>
-                      </span>
-                  </div>
-                  <input class="form-control" type="search" placeholder="Search" aria-label="Search">
-              </div>
-          </div>
-          <div class="col-md-4 mb-3">
-              <label for="validationCustom01">First name</label>
-              <input type="text" class="form-control" id="validationCustom01" placeholder="First name" value="Mark" required>
-              <div class="valid-feedback">
-                  Looks good!
-              </div>
-          </div>
-          <div class="col-md-4 mb-3">
-              <label for="validationCustom02">Last name</label>
-              <input type="text" class="form-control" id="validationCustom02" placeholder="Last name" value="Otto" required>
-              <div class="valid-feedback">
-                  Looks good!
-              </div>
-          </div>
-      </div>
-      <div class="form-row">
-          <div class="col-md-6 mb-3">
-              <label for="validationCustom03">City</label>
-              <input type="text" class="form-control" id="validationCustom03" placeholder="City" required>
-              <div class="invalid-feedback">
-                  Please provide a valid city.
-              </div>
-          </div>
-          <div class="col-md-3 mb-3">
-              <label for="validationCustom04">State</label>
-              <input type="text" class="form-control" id="validationCustom04" placeholder="State" required>
-              <div class="invalid-feedback">
-                  Please provide a valid state.
-              </div>
-          </div>
-          <div class="col-md-3 mb-3">
-              <label for="validationCustom05">Zip</label>
-              <input type="text" class="form-control" id="validationCustom05" placeholder="Zip" required>
-              <div class="invalid-feedback">
-                  Please provide a valid zip.
-              </div>
-          </div>
-      </div>
-  </form>
-  <div>
-  `,
+            </div>
+            <div class="col-md-3 mb-3">
+                <label for="validationCustom02">Worker availability</label>
+                <div ngbDropdown class="d-inline-block w-100">
+                    <app-select [btnText]=availabilityText [dropDownOptions]=availabilityOptions deactivate="true"></app-select>
+                </div>
+            </div>
+            <div class="col-md-3 mb-3">
+                <label for="validationCustom05">Worker name</label>
+                <input type="text" class="form-control" id="validationCustom05" placeholder="Name">
+            </div>
+        </div>
+    </form>
+    <div>
+        `,
 })
 export class SearchComponent {
+    private specialtiesOptions = [ 'Frontend developer', 'Backend developer', 'Tutor'];
+    private priceOptions = ['Any', '10.00', '15.00', '20.00', '50.00', '60.00'];
+    private availabilityOptions = ['Weekends', 'Week Days'];
+    private maxPriceOptions = this.priceOptions;
+    private minPriceText = 'Min price per hour';
+    private maxPriceText = 'Max price per hour';
+    private specialtiesText = 'Select the specialty';
+    private availabilityText = 'Select the worker availability';
+
+
+    calcMaxPriceOptions(minPrice) {
+        this.maxPriceOptions = [];
+        if (minPrice !== 'Any') {
+            from(this.priceOptions).pipe(
+                filter( num => num >= minPrice )
+            ).subscribe( res => {
+                this.maxPriceOptions.push(res);
+            });
+        }
+    }
 }
-
-
-
