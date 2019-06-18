13a3657945c5a70a8c645d11ddb76a27866cae0b
bgalbadrakh <a-bgalbadrakh@expedia.com>
2019-06-18 09:26:38 -0500

    added a table shows jobs list

--- frontend/src/app/components/job.component.ts
+++ frontend/src/app/components/job.component.ts
@@ -7,6 +7,7 @@ import { JobService } from '../services/job.service';
   template: `
 
   <div class="container">
+  <div  *ngIf="show_dialog">
     <h1>Adding a new job</h1>
     <form [formGroup]="myForm" (ngSubmit)="onSubmit()">
       <div class="form-group">
@@ -22,24 +23,60 @@ import { JobService } from '../services/job.service';
       <button type="submit" class="btn btn-primary" >Add</button>
 
     </form>
-</div>
+    </div>
+    <div  *ngIf="show_response">
+    <p>{{response_txt}}</p>
+    </div>
+    <br/>
+    <h3>Job list</h3>
+    <table class="table table-striped">
+      <thead class="grey lighten-1 black-text">
+        <tr>
+          <th >Id</th>
+          <th >Title</th>
+          <th >Description</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr mdbTableCol *ngFor="let job of jobs">
+            <th scope="row">{{job.id}}</th>
+            <td>{{job.title}}</td>
+            <td>{{job.description}}</td>
+          </tr>
+        </tbody>
+      </table>
+  </div>
 
   `,
 })
 export class JobComponent {
   myForm: FormGroup;
+  show_dialog: boolean = true;
+  show_response: boolean = false;
+  response_txt: String;
+  jobs: any;
 
     constructor(private saveJobService: JobService, private fb: FormBuilder) {
       this.myForm = fb.group({
         'title': '',
         'description': ''
       });
+      this.loadingJobs();
     }
     onSubmit() {
       this.saveJobService.saveJob(this.myForm.value).subscribe((res) => {
-        console.log("Form Submitted!");
+        this.response_txt = "A job added successfully";
       }, (err) => {
-        console.log("Form Failed!"), err;
+        this.response_txt = "Could not add a job!";
+      });
+      this.show_dialog = false;
+      this.show_response = true;
+    }
+    loadingJobs() {
+      this.saveJobService.getJobs().subscribe((res) => {
+      this.jobs = res['results']; 
+      }, (err) => {
+        console.log('error', err);
       });
     }
 }
